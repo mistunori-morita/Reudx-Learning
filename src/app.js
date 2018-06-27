48c02244224dd1cc680959b3c3660696e41c2425
mitsunori.morita <mitsunori.morita@speee.jpmitsunori.morita@speee.jpmitsunori.morita@speee.jp>
2018-06-27 15:33:51 +0900

    Redux setting

--- src/app.js
+++ src/app.js
@@ -2,12 +2,39 @@
 import {createStore} from 'redux'
 
 //spte3 define reducers
-const reducer = (state, action) => {
+const reducer = (state={books:[]}, action) => {
+
   switch(action.type){
     case "POST_BOOK":
-    return state = action.payload;
+    // let books = state.books.concat(action.payload)
+    // return {books};
+    return {books: [...state.books,...action.payload]}
     break;
 
+    case "DELETE_BOOK":
+    const currentBookToDelete = [...state.books]
+
+    const indexToDelte = currentBookToDelete.findIndex((book) =>{
+      return book.id === action.payload.id
+    } )
+    return {books: [...currentBookToDelete.slice(0,indexToDelte),...currentBookToDelete.slice(indexToDelte + 1)]}
+    break;
+
+    case "UPDATE_BOOK":
+      const currentBookToUpdate = [...state.books]
+      const indexToUpdate = currentBookToUpdate.findIndex((book) => {
+        return book.id === action.payload.id;
+      })
+
+      const newBookToUpdate = {
+        ...currentBookToUpdate[indexToUpdate],
+        title: action.payload.title
+      }
+
+      console.log("what is it newBookToUpdate", newBookToUpdate);
+
+      return {books: [...currentBookToUpdate.slice(0, indexToUpdate), newBookToUpdate, ...currentBookToUpdate.slice(indexToUpdate + 1)]}
+      break;
   }
   return state
 }
@@ -18,15 +45,40 @@ const store = createStore(reducer);
 
 store.subscribe(() => {
   console.log('current state is :' , store.getState())
-  console.log('current price :' , store.getState().price)
+  // console.log('current price :' , store.getState()[1].price)
 })
 
 //step 2 create and dispatch actions
 store.dispatch({
-  type: "POST_BOOK", payload: {
-    id: 1,
-    title: 'this is the book title',
-    description: 'this is the book description',
-    price: 33.33
+  type: "POST_BOOK",
+   payload: [
+     {
+      id: 1,
+      title: 'this is the book title',
+      description: 'this is the book description',
+      price: 33.33
+     },
+     {
+      id: 2,
+      title: 'this is the second book title',
+      description: 'this is the book description',
+      price: 50
+     },
+  ]
+})
+
+//DELETE a second action
+store.dispatch({
+  type: "DELETE_BOOK",
+  payload:{
+    id: 1
+  }
+})
+//Update a second action
+store.dispatch({
+  type: "UPDATE_BOOK",
+  payload:{
+    id:2,
+    title: 'Learn React'
   }
 })
